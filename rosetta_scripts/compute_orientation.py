e99a84855d675a3344b47a298c2556b5f0add6a7
Jesse Mapel <jmapel@usgs.gov>
2018-09-15 21:15:58 -0700

    Added computation to get north up rotation from ground point.

--- rosetta_scripts/compute_orientation.py
+++ rosetta_scripts/compute_orientation.py
@@ -13,10 +13,51 @@ def compute_position(ground_point, distance):
     bf_position = np.array(distance / np.linalg.norm(ground_point)) * ground_point
     return bf_position
 
-# TODO actually compute this properly
+def rotation_between(u, v, fallback=None):
+    tolerance=1e-10
+
+    unit_u = u / np.linalg.norm(u)
+    unit_v = v / np.linalg.norm(v)
+
+    dot = np.dot(unit_u,unit_v)
+    # If the vectors are within tolerance of parallel, return identity rotation
+    if dot >  1.0 - tolerance:
+        return np.quaternion(1,0,0,0)
+    # If the vectors are within tolerance of opposite, return a 180 degree rotation
+    # about an arbitrary perpendicular axis.
+    # If no fall back axis is provided,
+    # try the cross product of the x-axis and u.
+    # If they are co-linear use the y-axis.
+    elif dot < -1.0 + tolerance:
+        if fallback:
+            axis = fallback
+        else:
+            axis = np.cross(unit_u, np.array([1,0,0]))
+            if np.linalg.norm(axis) < tolerance:
+                axis = np.cross(unit_u, np.array([0,1,0]))
+        return np.quaternion(0, axis[0], axis[1], axis[2])
+
+    axis = np.cross(unit_u, unit_v)
+    scalar = 1 + dot
+    return np.quaternion(scalar, axis[0], axis[1], axis[2])
+
 def compute_rotation(ground_point):
-    bf_instrument_rotation = np.quaternion(1,0,0,0)
-    return bf_instrument_rotation
+    x_plus = np.array([1,0,0])
+    z_plus = np.array([0,0,1])
+    look_vector = -ground_point / np.linalg.norm(ground_point)
+
+    # First compute the rotation that takes +Z to the look vector
+    look_rotation = rotation_between(z_plus, -look_vector)
+
+    # Next compute the rotation that aligns North up, the rotation that takes
+    # the rotated +X to the component of +Z that is orthogonal the look vector.
+    rotated_x = quaternion.as_rotation_matrix(look_rotation) * x_plus)
+    north_up = z_plus - np.dot(z_plus, look_vector) * look_vector
+    # We need to make sure that if the rotated x and north up vector are
+    # opposites, then we use a 180 rotation about the look vector so as
+    # to not screw it up
+    north_rotation = rotation_between(rotated_x, north_up, look_vector)
+    return north_rotation * look_vector
 
 # TODO replace getkey calls with PVL library
 def get_key(cube, name, object=None, group=None):
